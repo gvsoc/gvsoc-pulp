a0d03167199f912c6b7faf71bea4ccc2a2fd3eb9
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2022-10-08 22:15:24 +0200

    [GENERATORS] Ported to new gapy

--- generators/pulp_open/pulp_open_board.py
+++ generators/pulp_open/pulp_open_board.py
@@ -18,63 +18,38 @@ from devices.flash.hyperflash import Hyperflash
 from devices.flash.spiflash import Spiflash
 from devices.flash.atxp032 import Atxp032
 from devices.ram.hyperram import Hyperram
-from tools.runner.runner import Runner_target
 from devices.testbench.testbench import Testbench
+import gv.gvsoc_runner
+from gapylib.chips.pulp.flash import *
 
 
-class Pulp_open_board(Runner_target):
+class Pulp_open_board(gv.gvsoc_runner.Runner):
 
-    def __init__(self, parent, name, ram_type: str='s27ks0641', ram_itf: int=0, ram_cs: int=0,
-            flash_type: str='spiflash', flash_itf: int=0, flash_cs: int=0):
+    def __init__(self, options, parent=None, name='top'):
 
-        super(Pulp_open_board, self).__init__(parent, name, boot_mode="flash", boot_device="flash")
-
-        # Register all parameters as properties so that they can be overwritten from the command-line
-        self.add_property('ram_type', ram_type)
-        self.add_property('ram_itf', ram_itf)
-        self.add_property('ram_cs', ram_cs)
-        self.add_property('flash_type', flash_type)
-        self.add_property('flash_itf', flash_itf)
-        self.add_property('flash_cs', flash_cs)
-
-        self.add_properties({
-            "runner": {
-                "modules": {
-                    "gvsoc": "gv.chips.pulp"
-                }
-            }
-        })
-
+        super(Pulp_open_board, self).__init__(parent, name, options=options)
 
         # Pulp
         pulp = Pulp_open(self, 'chip')
 
         # Flash
-        if flash_type == 's26ks512s':
-            flash = Hyperflash(self, 'flash')
+        hyperflash = Hyperflash(self, 'hyperflash')
+        spiflash = Spiflash(self, 'spiflash')
+        hyperram = Hyperram(self, 'ram')
 
-        elif flash_type == 'spiflash':
-            flash = Spiflash(self, 'flash')
+        self.register_flash(
+            DefaultFlashRomV2(self, 'flash', image_name=spiflash.get_image_path(),
+                flash_attributes={
+                    "flash_type": 'spi'
+                },
+                size=8*1024*1024
+            ))
 
-        elif flash_type == 'atxp032':
-            flash = Atxp032(self, 'flash')
+        self.bind(pulp, 'spim0_cs0', spiflash, 'cs')
+        self.bind(pulp, 'spim0_cs0_data', spiflash, 'input')
 
-        else:
-            raise RuntimeError('Unknown flash type: ' + flash_type)
+        self.bind(pulp, 'hyper0_cs1', hyperflash, 'cs')
+        self.bind(pulp, 'hyper0_cs1_data', hyperflash, 'input')
 
-        if flash_type == 'spiflash':
-            self.bind(pulp, 'spim%d_cs%d' % (flash_itf, flash_cs), flash, 'cs')
-            self.bind(pulp, 'spim%d_cs%d_data' % (flash_itf, flash_cs), flash, 'input')
-        else:
-            self.bind(pulp, 'hyper%d_cs%d' % (flash_itf, flash_cs), flash, 'cs')
-            self.bind(pulp, 'hyper%d_cs%d_data' % (flash_itf, flash_cs), flash, 'input')
-
-        # Ram
-        if ram_type == 's27ks0641':
-            ram = Hyperram(self, 'ram')
-
-        else:
-            raise RuntimeError('Unknown ram type: ' + ram_type)
-
-        self.bind(pulp, 'hyper%d_cs%d' % (ram_itf, ram_cs), ram, 'cs')
-        self.bind(pulp, 'hyper%d_cs%d_data' % (ram_itf, ram_cs), ram, 'input')
+        self.bind(pulp, 'hyper0_cs0', hyperram, 'cs')
+        self.bind(pulp, 'hyper0_cs0_data', hyperram, 'input')
