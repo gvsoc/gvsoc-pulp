7de91a91d1bdd3a00faee5b441bec2e6364fd018
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2023-09-26 17:04:56 +0200

    [SPATZ] Added tcdm and cluster registers

--- models/pulp/spatz/spatz.py
+++ models/pulp/spatz/spatz.py
@@ -22,7 +22,9 @@ import interco.router as router
 import utils.loader.loader
 import gsystree as st
 from interco.bus_watchpoint import Bus_watchpoint
+from pulp.spatz.cluster_registers import Cluster_registers
 from elftools.elf.elffile import *
+import gv.gvsoc_runner as gvsoc
 
 
 GAPY_TARGET = True
@@ -46,10 +48,15 @@ class Soc(st.Component):
 
         mem = memory.Memory(self, 'mem', size=0x1000000)
 
+        mem = memory.Memory(self, 'tcdm', size=0x40000)
+
         ico = router.Router(self, 'ico')
 
         ico.add_mapping('mem', base=0x80000000, remove_offset=0x80000000, size=0x1000000)
         self.bind(ico, 'mem', mem, 'input')
+
+        ico.add_mapping('tcdm', base=0x100000, remove_offset=0x100000, size=0x40000)
+        self.bind(ico, 'tcdm', mem, 'input')
 
         ico.add_mapping('rom', base=0x00001000, remove_offset=0x00001000, size=0x10000)
         self.bind(ico, 'rom', rom, 'input')
@@ -58,12 +65,15 @@ class Soc(st.Component):
 
         loader = utils.loader.loader.ElfLoader(self, 'loader', binary=binary, entry=0x1000)
 
+
         # RISCV bus watchpoint
         tohost_addr = 0
         fromhost_addr = 0
+        entry = 0
         if binary is not None:
             with open(binary, 'rb') as file:
                 elffile = ELFFile(file)
+                entry = elffile['e_entry']
                 for section in elffile.iter_sections():
                     if isinstance(section, SymbolTableSection):
                         for symbol in section.iter_symbols():
@@ -71,6 +81,10 @@ class Soc(st.Component):
                                 tohost_addr = symbol.entry['st_value']
                             if symbol.name == 'fromhost':
                                 fromhost_addr = symbol.entry['st_value']
+
+        cluster_registers = Cluster_registers(self, 'cluster_registers', boot_addr=entry)
+        ico.add_mapping('cluster_registers', base=0x00120000, remove_offset=0x00120000, size=0x1000)
+        self.bind(ico, 'cluster_registers', cluster_registers, 'input')
 
         tohost = Bus_watchpoint(self, 'tohost', tohost_addr, fromhost_addr, word_size=32)
         self.bind(host, 'data', tohost, 'input')
@@ -87,11 +101,11 @@ class Soc(st.Component):
 
 
 
-class Target(gv.gvsoc_runner.Runner):
+class Spatz(st.Component):
 
-    def __init__(self, parser, options):
+    def __init__(self, parent, name, parser, options):
 
-        super(Target, self).__init__(parser=parser, parent=None, name='top', options=options)
+        super(Spatz, self).__init__(parent, name, options=options)
 
         clock = Clock_domain(self, 'clock', frequency=50000000)
 
@@ -100,5 +114,9 @@ class Target(gv.gvsoc_runner.Runner):
         self.bind(clock, 'out', soc, 'clock')
 
 
-    def __str__(self) -> str:
-        return "RV32 virtual board"
+class Target(gvsoc.Target):
+
+    def __init__(self, parser, options):
+        super(Target, self).__init__(parser, options,
+            model=Spatz, description="Spatz virtual board")
+
