82c05874323058204c46a1a286a8a6a0df6ba61c
belanoa <123367320+belanoa@users.noreply.github.com>
2023-12-15 10:42:06 +0100

    Update setup.sh

--- models/pulp/redmule/run/setup.sh
+++ models/pulp/redmule/run/setup.sh
@@ -1,5 +1,6 @@
-export PULP_RISCV_GCC_TOOLCHAIN=/home/andrea/pulp_toolchain
-export GVSOC_PATH=/home/andrea/src/gvsoc
-export REDMULE_PATH=/home/andrea/src/gvsoc/pulp/models/pulp/redmule
-export PULP_SDK_PATH=/home/andrea/src/pulp-sdk
-export TEST_PATH=/home/andrea/src/gvsoc/pulp/models/pulp/redmule/test
+#To avoid passing the paths as an argument every time make is run, set your paths here and source this script to setup the global variables used in the Makefile
+export PULP_RISCV_GCC_TOOLCHAIN=
+export GVSOC_PATH=
+export REDMULE_PATH=
+export PULP_SDK_PATH=
+export TEST_PATH=
