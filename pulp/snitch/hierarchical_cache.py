82947b0f9c87ac3cf6d7acaa3818122c16f1e7da
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2024-04-22 11:05:46 +0200

    [CACHE] Added possibility to control the enable from generator

--- pulp/snitch/hierarchical_cache.py
+++ pulp/snitch/hierarchical_cache.py
@@ -46,12 +46,12 @@ class Hierarchical_cache(gvsoc.systree.Component):
         # L0 caches
         l0_caches = []
         for i in range(0, nb_pes):
-            l0_caches.append(Cache(self, 'l0_bank%d' % i, nb_sets_bits=0, nb_ways_bits=0, line_size_bits=5, refill_latency=0))
+            l0_caches.append(Cache(self, 'l0_bank%d' % i, nb_sets_bits=0, nb_ways_bits=0, line_size_bits=5, refill_latency=0, enabled=True))
         
         # L1 caches
         l1_caches = []
         for i in range(0, nb_l1_banks):
-            l1_caches.append(Cache(self, 'l1_bank%d' % i, nb_sets_bits=7, nb_ways_bits=1, line_size_bits=5, refill_latency=2, refill_shift=nb_l1_banks_log2, add_offset=i*l1_cache_line_size))
+            l1_caches.append(Cache(self, 'l1_bank%d' % i, nb_sets_bits=7, nb_ways_bits=1, line_size_bits=5, refill_latency=2, refill_shift=nb_l1_banks_log2, add_offset=i*l1_cache_line_size, enabled=True))
 
         # L1 interleaver
         interleaver = Interleaver(self, 'interleaver', nb_slaves=nb_l1_banks, nb_masters=0, interleaving_bits=l1_line_size_bits)
