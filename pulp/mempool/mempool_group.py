5be16d4eaa3453b77b6b0cfb7dacf9afe95d8271
Yichao Zhang <yiczhang@iis.ee.ethz.ch>
2024-05-15 18:55:15 +0200

    [Python] Unify the loader and CSR, move it to the system level for correct connection

--- pulp/mempool/mempool_group.py
+++ pulp/mempool/mempool_group.py
@@ -54,9 +54,9 @@ class Group(st.Component):
         ##########              Design Components             ##########
         ################################################################
         # TIles
-        tile_list = []
+        self.tile_list = []
         for i in range(0, nb_tiles_per_group):
-            tile_list.append(Tile(self,f'tile_{i}',parser=parser,tile_id=i, group_id=group_id, nb_cores_per_tile=nb_cores_per_tile, 
+            self.tile_list.append(Tile(self,f'tile_{i}',parser=parser,tile_id=i, group_id=group_id, nb_cores_per_tile=nb_cores_per_tile, 
                 nb_groups=nb_groups, total_cores=total_cores, bank_factor=bank_factor))
 
         #Group local interconnect
@@ -77,39 +77,70 @@ class Group(st.Component):
         l2_router = router.Router(self, 'l2_router', bandwidth=64, latency=1)
         l2_router.add_mapping('output')
 
+        #csr router
+        csr_router = router.Router(self, 'csr_router', bandwidth=32, latency=1)
+        csr_router.add_mapping('output')
+
+        #dummy_mem router
+        dummy_mem_router = router.Router(self, 'dummy_mem_router', bandwidth=32, latency=1)
+        dummy_mem_router.add_mapping('output')
+
         ################################################################
         ##########               Design Bindings              ##########
         ################################################################
 
         #Tile local master -> Group local interconnect
         for i in range(0, nb_tiles_per_group):
-            self.bind(tile_list[i], 'grp_local_master_out', group_local_interleaver, 'in_%d' % i)
+            self.bind(self.tile_list[i], 'grp_local_master_out', group_local_interleaver, 'in_%d' % i)
 
         #Group local interconnect -> Tile local slave
         for i in range(0, nb_tiles_per_group):
-            self.bind(group_local_interleaver, 'out_%d' % i, tile_list[i], 'grp_local_slave_in')
+            self.bind(group_local_interleaver, 'out_%d' % i, self.tile_list[i], 'grp_local_slave_in')
 
         #Tile remote master -> Group remote routers
         for port in range(0, nb_remote_ports):
             for i in range(0, nb_tiles_per_group):
-                self.bind(tile_list[i], f'grp_remt{port}_master_out', group_remote_master_interfaces[port], 'in_%d' % i)
+                self.bind(self.tile_list[i], f'grp_remt{port}_master_out', group_remote_master_interfaces[port], 'in_%d' % i)
 
         #Tile rom -> rom router
         for i in range(0, nb_tiles_per_group):
-            self.bind(tile_list[i], 'rom', rom_router, 'input')
+            self.bind(self.tile_list[i], 'rom', rom_router, 'input')
 
         #Tile l2 data -> l2 router
         for i in range(0, nb_tiles_per_group):
-            self.bind(tile_list[i], 'L2_data', l2_router, 'input')
+            self.bind(self.tile_list[i], 'L2_data', l2_router, 'input')
+        
+        #Tile l2 data -> csr router
+        for i in range(0, nb_tiles_per_group):
+            self.bind(self.tile_list[i], 'csr', csr_router, 'input')
+        
+        #Tile l2 data -> dummy_mem router
+        for i in range(0, nb_tiles_per_group):
+            self.bind(self.tile_list[i], 'dummy_mem', dummy_mem_router, 'input')
+        
+        #Group loader -> Tile loader
+        for i in range(0, nb_tiles_per_group):
+            self.bind(self, 'loader_start', self.tile_list[i], 'loader_start')
+            self.bind(self, 'loader_entry', self.tile_list[i], 'loader_entry')
 
         ################################################################
         ##########               Group Interfaces             ##########
         ################################################################
 
+        # Remote TCDM interface between tiles to the group
         for port in range(0, nb_remote_ports):
             for i in range(0, nb_tiles_per_group):
-                self.bind(self, f'grp_remt{port}_tile{i}_slave_in', tile_list[i], f'grp_remt{port}_slave_in')
+                self.bind(self, f'grp_remt{port}_tile{i}_slave_in', self.tile_list[i], f'grp_remt{port}_slave_in')
                 self.bind(group_remote_master_interfaces[port], 'out_%d' % i, self, f'grp_remt{port}_tile{i}_master_out')
 
+        # Propagate the barrier signals from the tiles to the group boundary
+        for i in range(0, nb_tiles_per_group):
+            for j in range(0, nb_cores_per_tile):
+                self.bind(self.tile_list[i], f'barrier_req_{j}', self, f'barrier_req_{i*nb_cores_per_tile+j}')
+                self.bind(self, f'barrier_ack_{i*nb_cores_per_tile+j}', self.tile_list[i], f'barrier_ack_{j}')
+
+        # Other signals propagated to the group interface
         self.bind(rom_router, 'output', self, 'rom')
         self.bind(l2_router, 'output', self, 'L2_data')
+        self.bind(csr_router, 'output', self, 'csr')
+        self.bind(dummy_mem_router, 'output', self, 'dummy_mem')
