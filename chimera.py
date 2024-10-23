80973b7254e2a6d61ad7a24068c5a71353a4ec55
Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
2024-10-23 17:30:22 +0200

    [memory-island] added a memory island model. verification is pending. pushing it only for backup

--- chimera.py
+++ chimera.py
@@ -13,6 +13,49 @@ from pulp.padframe.padframe_v1 import Padframe
 from utils.clock_generator import Clock_generator
 import pulp.soc_eu.soc_eu_v2 as soc_eu_module
 import pulp.itc.itc_v1 as itc
+import math
+
+class MemoryIsland(gvsoc.systree.Component):
+    def __init__(self, parent, name, config):
+        super(MemoryIsland, self).__init__(parent, name)
+        self.add_properties(config)            
+        nb_wide_banks               =  self.get_property("properties/nb_wide_banks", int)
+        nb_narrow_per_wide_bank     =  self.get_property("properties/nb_narrow_per_wide_bank", int)
+        memory_size                 =  self.get_property("mapping/size", int)
+        narrow_word_size            =  self.get_property("properties/narrow_word_size", int)
+        wide_word_size              =  int(narrow_word_size*nb_narrow_per_wide_bank)
+        nb_narrow_banks             =  int(nb_wide_banks * nb_narrow_per_wide_bank)
+        memory_size_per_narrow_bank =  memory_size // nb_narrow_banks  
+        nb_masters                  = 4
+        # Ensure memory size is a multiple of word size
+        assert memory_size                 % narrow_word_size == 0, f"Memory size {memory_size} is not a multiple of narrow word size {narrow_word_size}"
+        assert memory_size                 % wide_word_size   == 0, f"Memory size {memory_size} is not a multiple of wide word size {wide_word_size}"
+        assert memory_size                 % nb_narrow_banks  == 0, f"Memory size {memory_size} is not a multiple of number of narrow banks {nb_narrow_banks}"
+        assert memory_size_per_narrow_bank % narrow_word_size == 0, f"Memory size per bank {memory_size_per_narrow_bank} is not a multiple of number of narrow_word_size {narrow_word_size}"
+
+        # Memory banks corresponds to the narrow banks         
+        banks = []
+        for bank in range(0, nb_narrow_banks):
+            banks.append(memory.Memory(self, f'bank_{bank}', size=memory_size_per_narrow_bank, width_log2=int(math.log2(narrow_word_size))))
+        
+        from pulp.cluster.l1_interleaver import L1_interleaver
+        from pulp.snitch.snitch_cluster.dma_interleaver import DmaInterleaver # using the wide interleaver from Snitch, priority is always wide
+        # print(f"numer of narrow banks")
+        narrow_interleaver = L1_interleaver(self, 'narrow_interleaver',    nb_slaves = nb_narrow_banks, nb_masters = nb_masters, interleaving_bits=int(math.log2(narrow_word_size)))
+        wide_interleaver   = DmaInterleaver(self,  slave= nb_narrow_banks, nb_master_ports=nb_masters,  nb_banks=nb_narrow_banks, bank_width=narrow_word_size)
+
+        for bank in range(0, nb_narrow_banks):
+            self.bind(narrow_interleaver, 'out_%d' % bank, banks[bank], 'input')
+            self.bind(wide_interleaver, 'out_%d' % bank, banks[bank], 'input')
+
+        for master in range(0, nb_masters):
+            self.bind(self, f'in_narrow_{master}', narrow_interleaver, f'in_{master}')
+            self.bind(self, f'in_wide_{master}', wide_interleaver, f'input')
+        
+    def i_NARROW_INPUT(self, port: int) -> gvsoc.systree.SlaveItf:
+        return gvsoc.systree.SlaveItf(self, f'in_narrow_{port}', signature='io')
+    def i_WIDE_INPUT(self, port: int) -> gvsoc.systree.SlaveItf:
+        return gvsoc.systree.SlaveItf(self, f'in_wide_{port}', signature='io')
 
 
 class SafetyIsland(gvsoc.systree.Component):
@@ -55,8 +98,8 @@ class SafetyIsland(gvsoc.systree.Component):
         # Create the memory and router components based on the configuration
         cluster_l1_placeholder = memory.Memory(self, 'cluster_l1_placeholder', size=self.cluster_l1_config["size"])
 
-        # JUNGVI: Memory component placeholder for the memory island
-        memory_island_placeholder = memory.Memory(self, 'memory_island_placeholder', size=self.memory_island_config["size"])
+        # Memory component placeholder for the memory island
+        memory_island = MemoryIsland(self, 'memory_island', config=self.memory_island_config)
 
         l2_private_data_memory = memory.Memory(self, 'private_data_memory', size=self.memory_config["data"]["size"])
         l2_private_inst_memory = memory.Memory(self, 'private_inst_memory', size=self.memory_config["inst"]["size"])
@@ -73,11 +116,11 @@ class SafetyIsland(gvsoc.systree.Component):
         loader.o_ENTRY    ( host.i_ENTRY()        )
 
         # Mapping configurations for data, inst, and cluster memory
-        l2_tcdm_ico.o_MAP( l2_private_data_memory.i_INPUT(), "data_memory",          **self.get_property('memory_config/data'))
-        l2_tcdm_ico.o_MAP( l2_private_inst_memory.i_INPUT(), "inst_memory",          **self.get_property('memory_config/inst'))
-        l2_tcdm_ico.o_MAP( cluster_l1_placeholder.i_INPUT(),   "cluster_l1_placeholder", **self.get_property('cluster_l1_config'))
-        l2_tcdm_ico.o_MAP( obi_ico.i_INPUT(),                "obi_ico",              **self.get_property('obi_ico/mapping'))
-        l2_tcdm_ico.o_MAP( memory_island_placeholder.i_INPUT(),   "memory_island_placeholder", **self.get_property('memory_island_config'))
+        l2_tcdm_ico.o_MAP( l2_private_data_memory.i_INPUT(),              "data_memory",            **self.get_property('memory_config/data'))
+        l2_tcdm_ico.o_MAP( l2_private_inst_memory.i_INPUT(),              "inst_memory",            **self.get_property('memory_config/inst'))
+        l2_tcdm_ico.o_MAP( cluster_l1_placeholder.i_INPUT(),              "cluster_l1_placeholder", **self.get_property('cluster_l1_config'))
+        l2_tcdm_ico.o_MAP( obi_ico.i_INPUT(),                             "obi_ico",                **self.get_property('obi_ico/mapping'))
+        l2_tcdm_ico.o_MAP( memory_island.i_NARROW_INPUT("l2_ico_master"), "memory_island",          **self.get_property('memory_island_config/mapping'))
 
         obi_ico.add_mapping( 'soc_ctrl'   , **self.get_property('obi_ico/soc_ctrl'   ))             
         obi_ico.add_mapping( 'fll_soc'    , **self.get_property('obi_ico/fll_soc'    ))             
