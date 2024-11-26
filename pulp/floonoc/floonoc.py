7d95df8c02003baeee3deb1118da11b5d723c512
Jonas Martin <martinjo@student.ethz.ch>
2024-11-26 11:44:04 +0100

    move sending to target into the NI (no added delay)

--- pulp/floonoc/floonoc.py
+++ pulp/floonoc/floonoc.py
@@ -179,8 +179,12 @@ class FlooNocClusterGrid(FlooNoc2dMesh):
         for tile_x in range(0, nb_x_clusters):
             for tile_y in range(0, nb_y_clusters):
                 # Add 1 as clusters, routers and network_interfaces are in the central part
-                self.add_router(tile_x+1, tile_y+1)
-                self.add_network_interface(tile_x+1, tile_y+1)
+                self.add_router(tile_x+1, tile_y+1) # Add a router at each cluster
+        for tile_x in range(0, nb_x_clusters+2):
+            for tile_y in range(0, nb_y_clusters+2):
+                # Add a NI at each node, excluding the corners, because it also (once finished) acts as an output to the targets
+                if not (tile_x == 0 and tile_y == 0) or (tile_x == 0 and tile_y == nb_y_clusters+1) or (tile_x == nb_x_clusters+1 and tile_y == 0) or (tile_x == nb_x_clusters+1 and tile_y == nb_y_clusters+1):
+                    self.add_network_interface(tile_x, tile_y)
 
     def i_CLUSTER_INPUT(self, x: int, y: int) -> gvsoc.systree.SlaveItf:
         """Returns the input port of a cluster tile.
