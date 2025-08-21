aba845596270371b0bb13a684734d61839b98f0c
Yinrong Li <yinrli@student.ethz.ch>
2025-08-21 19:12:24 +0200

    [teranoc] Fix copyright issue.

--- pulp/teranoc/l1_noc.py
+++ pulp/teranoc/l1_noc.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 GreenWaves Technologies
+# Copyright (C) 2025 ETH Zurich and University of Bologna
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
