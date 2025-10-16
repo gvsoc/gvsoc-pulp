b4e9ca79989f6681fb6c7a1ff4e23a3bbf54cfc2
TheSSDGuy <lorzuo@gmail.com>
2025-10-16 11:16:42 +0200

    Fixed merge issues with floonoc files

--- pulp/floonoc/floonoc.cpp
+++ pulp/floonoc/floonoc.cpp
@@ -214,8 +214,7 @@ void FlooNoc::reset(bool active)
 
 
 
-//Entry *FlooNoc::get_entry(uint64_t base, uint64_t size)
-Entry *FlooNoc::get_entry(uint64_t base, uint64_t size, int x, int y)
+Entry *FlooNoc::get_entry(uint64_t base, uint64_t size)
 {
     // For now, we store mapping in a classic array.
     // Just go through each entry one by one, until one is matching the requested memory location
@@ -225,14 +224,7 @@ Entry *FlooNoc::get_entry(uint64_t base, uint64_t size, int x, int y)
         // We allow partial entry, the network interface will take care of splitting transactions
         if (base >= entry->base && base < entry->base + entry->size)
         {
-            if (entry->x==0) {
-                if (entry->y==y) {
-                    //printf("[ZL-MOD] Spotted access to L2. Source NI is x=%d,y=%d. Destination NI is x=%d,y=%d\n",x,y,entry->x,entry->y);
-                    return entry;
-                }
-            }
-            else 
-                return entry;
+            return entry;
         }
     }
     return NULL;
