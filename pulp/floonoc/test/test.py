657960658b76f4175464a659c8433cfa998af9dc
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2025-12-09 00:32:03 +0100

    floonoc: Add basic 3D support test

--- pulp/floonoc/test/test.py
+++ pulp/floonoc/test/test.py
@@ -27,11 +27,12 @@ GAPY_TARGET = True
 
 class FloonocTest(gvsoc.systree.Component):
 
-    def __init__(self, parent, name, nb_cluster_x, nb_cluster_y, cluster_base, cluster_size):
+    def __init__(self, parent, name, nb_cluster_x, nb_cluster_y, cluster_base, cluster_size, nb_cluster_z=1):
         super().__init__(parent, name)
 
         self.add_property('nb_cluster_x', nb_cluster_x)
         self.add_property('nb_cluster_y', nb_cluster_y)
+        self.add_property('nb_cluster_z', nb_cluster_z)
         self.add_property('cluster_base', cluster_base)
         self.add_property('cluster_size', cluster_size)
 
@@ -39,18 +40,19 @@ class FloonocTest(gvsoc.systree.Component):
         self.add_sources(['test0.cpp'])
         self.add_sources(['test1.cpp'])
         self.add_sources(['test2.cpp'])
+        self.add_sources(['test3d.cpp'])
 
-    def o_NOC_NI(self, x, y, itf: gvsoc.systree.SlaveItf):
-        self.itf_bind(f'noc_ni_{x}_{y}', itf, signature='io')
+    def o_NOC_NI(self, x, y, itf: gvsoc.systree.SlaveItf, z=0):
+        self.itf_bind(f'noc_ni_{x}_{y}_{z}', itf, signature='io')
 
-    def o_GENERATOR_CONTROL(self, x, y, itf: gvsoc.systree.SlaveItf):
-        self.itf_bind(f'generator_control_{x}_{y}', itf, signature='wire<TrafficGeneratorConfig>')
+    def o_GENERATOR_CONTROL(self, x, y, itf: gvsoc.systree.SlaveItf, z=0):
+        self.itf_bind(f'generator_control_{x}_{y}_{z}', itf, signature='wire<TrafficGeneratorConfig>')
 
-    def o_RECEIVER_CONTROL(self, x, y, itf: gvsoc.systree.SlaveItf):
-        self.itf_bind(f'receiver_control_{x}_{y}', itf, signature='wire<TrafficReceiverConfig>')
+    def o_RECEIVER_CONTROL(self, x, y, itf: gvsoc.systree.SlaveItf, z=0):
+        self.itf_bind(f'receiver_control_{x}_{y}_{z}', itf, signature='wire<TrafficReceiverConfig>')
 
-    def i_CLUSTER(self, x, y) -> gvsoc.systree.SlaveItf:
-        return gvsoc.systree.SlaveItf(self, f'cluster_{x}_{y}', signature='io')
+    def i_CLUSTER(self, x, y, z=0) -> gvsoc.systree.SlaveItf:
+        return gvsoc.systree.SlaveItf(self, f'cluster_{x}_{y}_{z}', signature='io')
 
 class Testbench(gvsoc.systree.Component):
 
@@ -59,29 +61,31 @@ class Testbench(gvsoc.systree.Component):
 
         nb_cluster_x = 3
         nb_cluster_y = 3
+        nb_cluster_z = 3
         cluster_base = 0x80000000
         cluster_size = 0x01000000
 
-        noc = pulp.floonoc.floonoc.FlooNocClusterGridNarrowWide(self, 'noc', 8, 8, nb_cluster_x, nb_cluster_y)
+        noc = pulp.floonoc.floonoc.FlooNocClusterGridNarrowWide(self, 'noc', 8, 8, nb_cluster_x, nb_cluster_y, nb_z_clusters=nb_cluster_z)
 
-        test = FloonocTest(self, 'test', nb_cluster_x, nb_cluster_y, cluster_base, cluster_size)
+        test = FloonocTest(self, 'test', nb_cluster_x, nb_cluster_y, cluster_base, cluster_size, nb_cluster_z=nb_cluster_z)
 
         for x in range(0, nb_cluster_x):
             for y in range(0, nb_cluster_y):
-                generator = interco.traffic.generator.Generator(self, f'generator_{x}_{y}')
-                generator.o_OUTPUT(noc.i_CLUSTER_WIDE_INPUT(x, y))
+                for z in range(0, nb_cluster_z):
+                    generator = interco.traffic.generator.Generator(self, f'generator_{x}_{y}_{z}')
+                    generator.o_OUTPUT(noc.i_CLUSTER_WIDE_INPUT(x, y, z=z))
 
-                receiver = interco.traffic.receiver.Receiver(self, f'receiver_{x}_{y}')
+                    receiver = interco.traffic.receiver.Receiver(self, f'receiver_{x}_{y}_{z}')
 
-                test.o_NOC_NI(x, y, noc.i_CLUSTER_WIDE_INPUT(x, y))
-                test.o_GENERATOR_CONTROL(x, y, generator.i_CONTROL())
-                test.o_RECEIVER_CONTROL(x, y, receiver.i_CONTROL())
+                    test.o_NOC_NI(x, y, noc.i_CLUSTER_WIDE_INPUT(x, y, z=z))
+                    test.o_GENERATOR_CONTROL(x, y, generator.i_CONTROL(), z=z)
+                    test.o_RECEIVER_CONTROL(x, y, receiver.i_CONTROL(), z=z)
 
-                noc.o_WIDE_MAP(
-                    receiver.i_INPUT(),
-                    cluster_base + cluster_size * (y * nb_cluster_x + x),
-                    cluster_size, x+1, y+1,
-                    name=f'ni_{x}_{y}')
+                    noc.o_WIDE_MAP(
+                        receiver.i_INPUT(),
+                        cluster_base + cluster_size * (z * nb_cluster_y * nb_cluster_x + y * nb_cluster_x + x),
+                        cluster_size, x+1, y+1,
+                        name=f'ni_{x}_{y}_{z}', z=z)
 
 
 # This is a wrapping component of the real one in order to connect a clock generator to it
