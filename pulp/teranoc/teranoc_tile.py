dcb8d778c92a0f4f71a5889a09782bf1f55d4cd9
Yinrong Li <yinrli@student.ethz.ch>
2026-01-09 15:06:12 +0100

    teranoc: configure SnitchFast core options

--- pulp/teranoc/teranoc_tile.py
+++ pulp/teranoc/teranoc_tile.py
@@ -102,7 +102,7 @@ class Tile(st.Component):
             if fast_model:
                 self.int_cores.append(iss.SnitchFast(self, f'pe{core_id}', isa="rv32imaf",
                     core_id=group_id*nb_tiles_per_group*nb_cores_per_tile+tile_id*nb_cores_per_tile+core_id,
-                    htif=False, pulp_v2=True, wakeup_counter=True, nb_outstanding=8
+                    htif=False, pulp_v2=True, wakeup_counter=True, nb_outstanding=8, single_regfile=True, ssr=False, sequencer=False
                 ))
             else:
                 self.int_cores.append(iss.Snitch(self, f'pe{core_id}', isa="rv32imaf", htif=False, \
