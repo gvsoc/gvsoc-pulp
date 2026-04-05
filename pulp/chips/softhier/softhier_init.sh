5e0f04a1019ae0bb81bf5017b2cb59731bfbcc49
Zhang Chi <chizhang@iis.ee.ethz.ch>
2026-04-05 17:09:18 +0200

    [Update] Softhier

--- pulp/chips/softhier/softhier_init.sh
+++ pulp/chips/softhier/softhier_init.sh
@@ -1,18 +1,2 @@
-if [  -n "${ZSH_VERSION:-}" ]; then
-    DIR="$(readlink -f -- "${(%):-%x}")"
-    SDK_HOME=$(dirname $DIR)
-else
-    SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-fi
-
-if [ -n "${GVSOC_WORKDIR}" ]; then
-    SDK_INSTALL=${GVSOC_WORKDIR}
-else
-    SDK_INSTALL=${SDK_HOME}
-fi
-
 make third_party/toolchain
-
-export PATH=$SDK_INSTALL/install/bin:$PATH
-export PYTHONPATH=$SDK_INSTALL/install/python:$PYTHONPATH
 export PATH=$SDK_HOME/third_party/toolchain/install/bin:$PATH
