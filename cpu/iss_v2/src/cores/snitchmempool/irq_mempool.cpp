1e0d5139791a46b7f93be164e40c8c06185ad6be
Yinrong Li <yinrli@student.ethz.ch>
2026-06-10 19:44:49 +0200

    cpu/iss_v2: add configurable SnitchMempool core

---
+++ cpu/iss_v2/src/cores/snitchmempool/irq_mempool.cpp
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2026 ETH Zurich, University of Bologna
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Authors: Germain Haugou (germain.haugou@gmail.com)
+
+#include <cpu/iss_v2/include/iss.hpp>
+
+void IrqMempool::wfi_handle(iss_insn_t *insn)
+{
+    // Consume a banked barrier wake-up if any; otherwise sleep.
+    if (this->iss.arch.wakeup_check())
+    {
+        return;
+    }
+
+    this->iss.exec.wfi.set(true);
+    this->iss.exec.wfi_start = this->iss.clock.get_cycles();
+    this->iss.exec.retain_inc();
+    this->wfi_entry = this->iss.exec.insn_hold(insn);
+}
