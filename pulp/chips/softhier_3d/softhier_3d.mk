bced88f37a65db702508ecbf85ee06b3add87812
ka1ew <s@DESKTOP-993AVQ2.localdomain>
2026-06-18 21:08:39 +0200

    Update makefile structure

--- pulp/chips/softhier_3d/softhier_3d.mk
+++ pulp/chips/softhier_3d/softhier_3d.mk
@@ -15,14 +15,14 @@ ifdef cfg
 	config_file_3d = "$(cfg)"
 endif
 
-sh-3d-config:
+sh-config:
 	@if [ "$(config_file_3d)" != "pulp/pulp/chips/softhier_3d/softhier_arch.py" ]; then \
 		cp -f $(config_file_3d) pulp/pulp/chips/softhier_3d/softhier_arch.py; \
 	fi
 	python3 pulp/pulp/chips/softhier_3d/utils/config.py $(config_file_3d)
 
-sh-3d-hw:
-	make sh-3d-config
+sh-hw:
+	make sh-config
 	make TARGETS=pulp.chips.softhier_3d.softhier_target all
 
 ######################################################################
@@ -37,12 +37,12 @@ endif
 
 arch_cmake_arg := "-DRISCV_ARCH=rv32imafdv_zfh"
 
-sh-3d-sw:
+sh-sw:
 	rm -rf sw_build && mkdir sw_build
 	cd sw_build && $(CMAKE) $(sw_cmake_arg) $(arch_cmake_arg) ../pulp/pulp/chips/softhier_3d/sw/ && make
 	@! grep -q "ebreak" sw_build/softhier.dump || (echo "Error: 'ebreak' found in sw_build/softhier.dump" && exit 1)
 
-sh-3d-sw-clean:
+sh-sw-clean:
 	rm -rf sw_build
 
 
@@ -50,28 +50,5 @@ sh-3d-sw-clean:
 ## 				Make Targets for Run Simulator		 				##
 ######################################################################
 
-sh-3d-run:
-	./install/bin/gvsoc --target=pulp.chips.softhier_3d.softhier_target --binary sw_build/softhier.elf run
-
-######################################################################
-##              Make Targets for Automated Test Suite               ##
-######################################################################
-
-# Define the base software directory (where CMakeLists.txt lives)
-SW_BASE_DIR = pulp/pulp/chips/softhier_3d/sw
-
-# Default to the test name if no TEST variable is provided
-TEST ?= 00_init
-
-# Automatically insert the /tests/ folder into the absolute path
-TEST_DIR = $(abspath $(SW_BASE_DIR)/tests/$(TEST))
-
-# Build target: Compiles whatever is inside $(TEST_DIR)
-sh-3d-test-sw:
-	rm -rf sw_build && mkdir sw_build
-	cd sw_build && $(CMAKE) "-DSRC_DIR=$(TEST_DIR)" $(arch_cmake_arg) ../$(SW_BASE_DIR) && make
-	@! grep -q "ebreak" sw_build/softhier.dump || (echo "Error: 'ebreak' found in sw_build/softhier.dump" && exit 1)
-
-# Run target: Executes the freshly built binary
-sh-3d-test-run: #sh-3d-test-sw
+sh-run:
 	./install/bin/gvsoc --target=pulp.chips.softhier_3d.softhier_target --binary sw_build/softhier.elf run
